f73bdc5cfcc721ae2cebd80c6c57eb0fe9d620e3
ZhouShuren <61644268+ZhouShuren1881@users.noreply.github.com>
2020-12-30 16:14:58 +0800

    310

--- public-test/src/test/java/cn/edu/xmu/oomall/goods/ShaoLiangYingTest.java
+++ public-test/src/test/java/cn/edu/xmu/oomall/goods/ShaoLiangYingTest.java
@@ -672,17 +672,17 @@ public class ShaoLiangYingTest {
                 .returnResult()
                 .getResponseBodyContent();
 
-        //时段冲突 /* -XxXxX - 修改时间使其大于当前时间 */
-        beginTime = LocalDateTime.now().plusDays(10);
-        endTime = beginTime.plusDays(5);
-        requestJson = "{\"activityPrice\":\"120\", \"beginTime\":\""+beginTime.toString()+"\",\"endTime\":\""+endTime.toString()+"\",\"quantity\": \"100\"}";
-        res = manageClient.post().uri("/shops/1/skus/626/floatPrices").header("authorization",adminToken).bodyValue(requestJson);
-        responseBuffer = res.exchange()
-                .expectBody()
-                .jsonPath("$.errno").isEqualTo(ResponseCode.SKUPRICE_CONFLICT.getCode())
-                .jsonPath("$.data").doesNotExist()
-                .returnResult()
-                .getResponseBodyContent();
+        //时段冲突 /* -XxXxX - 忽略，开始时间已经过期*/
+//        beginTime = LocalDateTime.parse("2020-12-28 17:42:20",df);
+//        endTime = LocalDateTime.parse("2021-01-28 17:42:20",df);
+//        requestJson = "{\"activityPrice\":\"120\", \"beginTime\":\""+beginTime.toString()+"\",\"endTime\":\""+endTime.toString()+"\",\"quantity\": \"100\"}";
+//        res = manageClient.post().uri("/shops/1/skus/626/floatPrices").header("authorization",adminToken).bodyValue(requestJson);
+//        responseBuffer = res.exchange()
+//                .expectBody()
+//                .jsonPath("$.errno").isEqualTo(ResponseCode.SKUPRICE_CONFLICT.getCode())
+//                .jsonPath("$.data").doesNotExist()
+//                .returnResult()
+//                .getResponseBodyContent();
     }
 
     /**
